cfa7cfb1183297bb80334bd9e2012c117b88b98e
Mitchell Stanley <mitchell.stanley@isev.co.uk>
2018-09-03 12:18:08 +0100

    Add implementation for familiar function

--- lib/name_of_person/person_name.ex
+++ lib/name_of_person/person_name.ex
@@ -11,6 +11,8 @@ defmodule NameOfPerson.PersonName do
 
       iex> NameOfPerson.PersonName.full("Mitch")
       "Mitch"
+      iex> NameOfPerson.PersonName.full("Mitch Stanley")
+      "Mitch Stanley"
       iex> NameOfPerson.PersonName.full("Mitch", "Stanley")
       "Mitch Stanley"
       iex> NameOfPerson.PersonName.full("Mitch", "Blank", "Stanley")
@@ -28,5 +30,34 @@ defmodule NameOfPerson.PersonName do
   def full(person = %NameOfPerson.PersonName{middle_name: ""}), do: String.trim("#{person.first_name} #{person.last_name}")
   def full(person = %NameOfPerson.PersonName{}), do: String.trim("#{person.first_name} #{person.middle_name} #{person.last_name}")
   def full(name), do: full(%NameOfPerson.PersonName{first_name: name})
+
+  @doc """
+  Returns first name and last initial, E.g. "Mitch S.".
+
+  ## Examples
+
+    iex> NameOfPerson.PersonName.familiar("Mitch", "Stanley")
+    "Mitch S."
+    iex> NameOfPerson.PersonName.familiar("Mitch Stanley")
+    "Mitch S."
+    iex> NameOfPerson.PersonName.familiar("Mitch Blank Stanley")
+    "Mitch S."
+    iex> NameOfPerson.PersonName.familiar(%NameOfPerson.PersonName{first_name: "Mitch", last_name: "Stanley"})
+    "Mitch S."
+  """
+  def familiar(first, last), do: familiar(%NameOfPerson.PersonName{first_name: first, last_name: last})
+  def familiar(first, middle, last), do: familiar(%NameOfPerson.PersonName{first_name: first, middle_name: middle, last_name: last})
+  def familiar([first, last]), do: familiar(%NameOfPerson.PersonName{first_name: first, last_name: last})
+  def familiar([first, middle, last]), do: familiar(%NameOfPerson.PersonName{first_name: first, last_name: last})
+  def familiar(person = %NameOfPerson.PersonName{last_name: ""}), do: "#{String.trim(person.first_name)}"
+  def familiar(person = %NameOfPerson.PersonName{}) do
+    "#{String.trim(person.first_name)} #{String.trim(String.first(person.last_name))}."
+  end
+  def familiar(name) do
+    String.split(name, " ", trim: true)
+    |> familiar()
+  end
+
+
 
 end
